b0f916da9b6b0eb60af0eefa2a01c682e79a655a
MohamedOuhami <mohamed.ouhami2001@gmail.com~>
2023-03-12 01:36:49 +0100

    Complete the Management, Dashboard ,Login system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SalleController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\MachineController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
 
@@ -15,14 +19,30 @@ use App\Http\Controllers\DashboardController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// The roor of the app
+Route::get('/',[DashboardController::class, 'index'])->name("dashboard")->middleware('auth');
 
 //  The sign-in Page
-Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::get('/login',[LoginController::class,'index'])->name('login')->middleware('guest');
 Route::post('/login',[LoginController::class,'store']);
 
+// Logging out
+Route::post('/logout',[LogoutController::class,'store'])->name("logout");
+
 // The Registration Page
-Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::get('/register',[RegisterController::class,'index'])->name('register')->middleware('guest');
 Route::post('/register',[RegisterController::class,'store']);
 
 // The dashboard Page
-Route::get('/dashboard',[DashboardController::class, 'index'])->name("dashboard");
+Route::get('/dashboard',[DashboardController::class, 'index'])->name("dashboard")->middleware('auth');
+
+// The Room Management Page
+Route::get('/room',[SalleController::class,'index'])->name('room');
+Route::post('/room',[SalleController::class,'store']);
+Route::put('/room/{id}',[SalleController::class,'update'])->name('room.update');
+Route::delete('/room/{id}', [SalleController::class, 'destroy'])->name('room.destroy');
+
+// The Machine Management PAge
+Route::get('/machine',[MachineController::class,'index'])->name('machine');
+Route::post('/machine',[MachineController::class,'store']);
+Route::delete('/machine/{id}', [MachineController::class, 'destroy'])->name('machine.destroy');
